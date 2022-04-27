757f2a71b878e144e25eefa765d6d25e5ec30e40
Hossain Mahmud <hmsayem@appscode.com>
2022-04-27 17:25:48 +0600

    Add support for cross-namespace target (#99)
    * Add support for cross-namespace target
    
    Signed-off-by: hmsayem <hmsayem@appscode.com>
    
    * Revendor
    
    Signed-off-by: hmsayem <hmsayem@appscode.com>

--- pkg/restore.go
+++ pkg/restore.go
@@ -81,6 +81,7 @@ func NewCmdRestore() *cobra.Command {
 				APIVersion: appcatalog.SchemeGroupVersion.String(),
 				Kind:       appcatalog.ResourceKindApp,
 				Name:       opt.appBindingName,
+				Namespace:  opt.appBindingNamespace,
 			}
 
 			var restoreOutput *restic.RestoreOutput
@@ -115,6 +116,7 @@ func NewCmdRestore() *cobra.Command {
 	cmd.Flags().StringVar(&kubeconfigPath, "kubeconfig", kubeconfigPath, "Path to kubeconfig file with authorization information (the master location is set by the master flag).")
 	cmd.Flags().StringVar(&opt.namespace, "namespace", "default", "Namespace of Backup/Restore Session")
 	cmd.Flags().StringVar(&opt.appBindingName, "appbinding", opt.appBindingName, "Name of the app binding")
+	cmd.Flags().StringVar(&opt.appBindingNamespace, "appbinding-namespace", opt.appBindingNamespace, "Namespace of the app binding")
 	cmd.Flags().StringVar(&opt.storageSecret.Name, "storage-secret-name", opt.storageSecret.Name, "Name of the storage secret")
 	cmd.Flags().StringVar(&opt.storageSecret.Namespace, "storage-secret-namespace", opt.storageSecret.Namespace, "Namespace of the storage secret")
 
@@ -154,7 +156,7 @@ func (opt *redisOptions) restoreRedis(targetRef api_v1beta1.TargetRef) (*restic.
 		return nil, err
 	}
 
-	appBinding, err := opt.catalogClient.AppcatalogV1alpha1().AppBindings(opt.namespace).Get(context.TODO(), opt.appBindingName, metav1.GetOptions{})
+	appBinding, err := opt.catalogClient.AppcatalogV1alpha1().AppBindings(opt.appBindingNamespace).Get(context.TODO(), opt.appBindingName, metav1.GetOptions{})
 	if err != nil {
 		return nil, err
 	}
