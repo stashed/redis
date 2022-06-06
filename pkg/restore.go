d34cf72bc317dbe2a09441cada2278db57807be4
piyush1146115 <piyush@appscode.com>
2022-06-06 11:19:10 +0600

    Refactor implementation.
    Signed-off-by: piyush1146115 <piyush@appscode.com>

--- pkg/restore.go
+++ pkg/restore.go
@@ -29,7 +29,6 @@ import (
 	"gomodules.xyz/flags"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	appcatalog "kmodules.xyz/custom-resources/apis/appcatalog/v1alpha1"
 	appcatalog_cs "kmodules.xyz/custom-resources/client/clientset/versioned"
@@ -65,6 +64,8 @@ func NewCmdRestore() *cobra.Command {
 			if err != nil {
 				return err
 			}
+			opt.config = config
+
 			opt.kubeClient, err = kubernetes.NewForConfig(config)
 			if err != nil {
 				return err
@@ -82,7 +83,7 @@ func NewCmdRestore() *cobra.Command {
 			}
 
 			var restoreOutput *restic.RestoreOutput
-			restoreOutput, err = opt.restoreRedis(targetRef, config)
+			restoreOutput, err = opt.restoreRedis(targetRef)
 			if err != nil {
 				restoreOutput = &restic.RestoreOutput{
 					RestoreTargetStatus: api_v1beta1.RestoreMemberStatus{
@@ -136,9 +137,9 @@ func NewCmdRestore() *cobra.Command {
 	return cmd
 }
 
-func (opt *redisOptions) restoreRedis(targetRef api_v1beta1.TargetRef, config *restclient.Config) (*restic.RestoreOutput, error) {
+func (opt *redisOptions) restoreRedis(targetRef api_v1beta1.TargetRef) (*restic.RestoreOutput, error) {
 	var err error
-	err = license.CheckLicenseEndpoint(config, licenseApiService, SupportedProducts)
+	err = license.CheckLicenseEndpoint(opt.config, licenseApiService, SupportedProducts)
 	if err != nil {
 		return nil, err
 	}
