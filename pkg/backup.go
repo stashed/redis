0acbd751a9797976ec99bcf6612cdc296dc14b07
Anisur Rahman <54911684+anisurrahman75@users.noreply.github.com>
2025-07-31 16:46:52 +0600

    Add Automatic Restic Unlock feature (#274)
    /cherry-pick
    
    Signed-off-by: Anisur Rahman <anisur@appscode.com>

--- pkg/backup.go
+++ pkg/backup.go
@@ -253,5 +253,10 @@ func (opt *redisOptions) backupRedis(targetRef api_v1beta1.TargetRef) (*restic.B
 	if err != nil {
 		return nil, err
 	}
+	err = resticWrapper.EnsureNoExclusiveLock(opt.kubeClient, opt.namespace)
+	if err != nil {
+		return nil, err
+	}
+
 	return resticWrapper.RunBackup(opt.backupOptions, targetRef)
 }
